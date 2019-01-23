9f1c0372f4ca9b4aaec55abecc0118caf26ba2a5
xiaomaxiao <514358041@qq.com>
2019-01-23 11:39:55 +0800

    ~

--- model/yolov3.py
+++ model/yolov3.py
@@ -3,6 +3,8 @@ from keras.layers import Conv2D , LeakyReLU , BatchNormalization, ZeroPadding2D 
 from keras.layers import UpSampling2D , Concatenate
 from keras.models import Model
 from keras.regularizers import l2
+import keras.backend as K
+import tensorflow as tf 
 
 class YOLO_V3():
     def __init__(self, *args, **kwargs):
@@ -51,7 +53,7 @@ class YOLO_V3():
 
         #predict
         y = self._conv_bn_leaky(x,filters*2,(3,3))
-        y = self._conv_bn_leaky(y,out_fliters,(1,1))
+        y = self.conv2d(y,out_fliters,(1,1))  #待写
         return x,y
 
     def yolo_body(self,input,num_anchors,num_class):
@@ -77,7 +79,130 @@ class YOLO_V3():
         return Model(input,[y1,y2,y3])
 
 
+    def yolo_head(self,feats,anchors,num_classes,input_shape,calc_loss = False):
+        '''
+        https://mp.weixin.qq.com/s/4L9E4WGSh0hzlD303036bQ
+        '''
+        num_anchors = len(anchors)
+        #reshape to batch,height,width,num_anchors,box_params
+        anchors_tensor= K.reshape(K.constant(anchors),[1,1,1,num_anchors,2]) #(1,1,1,3,2)
 
+        grid_shape = K.shape(feats)[1:3] #height,width即预测图feats的第1~2位，如13x13；
+        grid_y = K.tile(K.reshape(K.arange(0,stop=grid_shape[0]),[-1,1,1,1]),#(13,1,1,1)
+                        [1,grid_shape[1],1,1]) #(13,13,1,1)
 
+        grid_x = K.tile(K.reshape(K.arange(0,stop=grid_shape[1]),[1,-1,1,1]),  #(1,13,1,1)
+                                  [grid_shape[0],1,1,1])  #(13,13,1,1)
+        grid = K.concatenate([grid_x, grid_y])       #(13,13,1,2)
+        gird = K.cast(grid,K.dtype(feats))
+
+        feats = K.reshape(feats,[-1,grid_shape[0],gird_shape[1],num_anchors,num_classes+5])
 
         
+        box_xy = (K.sigmoid(feats[...,:2]) + grid) / K.cast(grid_shape[::-1],K.dtype(feats))#(1,13,13,3,2)
+        box_wh = K.exp(feats[...,2:4]) * anchors_tensor / K.cast(input_shape[::-1],K.dtype(feats))#(1,13,13,3,2)
+        box_confidence = K.sigmoid(feats[...,4:5])
+        box_class_prods = K.sigmoid(feats[...,5:])
+
+        if calc_loss ==True:
+            return grid,feats,box_xy,box_wh
+        return box_xy,box_wh,box_confidence,box_class_prods
+    
+    def box_iou(b1,b2):
+        '''Return iou tensor
+
+        Parameters
+        ----------
+        b1: tensor, shape=(i1,...,iN, 4), xywh
+        b2: tensor, shape=(j, 4), xywh
+
+        Returns
+        -------
+        iou: tensor, shape=(i1,...,iN, j)
+
+        '''
+        b1 = K.expand_dims(b1,-2)
+        b1_xy = b1[...,:2]
+        b1_wh = b1[...,2:4]
+        b1_wh_half = b1_wh /2.
+        b1_mins = b1_xy - b1_wh_half
+        b1_maxes = b1_xy + b1_wh_half
+
+        b2 = K.expand_dims(b1,0)
+        b2_xy = b2[...,:2]
+        b2_wh = b2[...,2:4]
+        b2_wh_half = b2_wh / 2.
+        b2_mins = b2_xy - b2_wh_half
+        b2_maxes = b2_xy + b2_wh_half
+        
+        intersect_mins = K.maximum(b1_mins,b2_mins)
+        intersect_maxes = K.minimum(b1_maxes,b2_maxes)
+        intersect_wh = K.maximum(intersect_maxes - intersect_mins, 0.)
+        intersect_area = intersect_wh[..., 0] * intersect_wh[..., 1]
+        b1_area = b1_wh[..., 0] * b1_wh[..., 1]
+        b2_area = b2_wh[..., 0] * b2_wh[..., 1]
+        iou = intersect_area / (b1_area + b2_area - intersect_area)
+
+        return iou
+
+    def yolo_loss(self,yolo_outputs,y_true,anchors,num_classes,ignore_thresh = 0.5 ,print_loss = False):
+        '''Return yolo_loss tensor
+
+        Parameters
+        ----------
+        yolo_outputs: list of tensor, the output of yolo_body or tiny_yolo_body
+        y_true: list of array, the output of preprocess_true_boxes
+        anchors: array, shape=(N, 2), wh
+        num_classes: integer
+        ignore_thresh: float, the iou threshold whether to ignore object confidence loss
+
+        Returns
+        -------
+        loss: tensor, shape=(1,)
+
+        '''
+        num_layers = len(anchors)//3
+        anchor_mask = [[6,7,8], [3,4,5], [0,1,2]] if num_layers==3 else [[3,4,5], [0,1,2]]
+        # input_shape是输出的尺寸*32, 就是原始的输入尺寸，[1:3]是尺寸的位置，即416x416
+        input_shape = K.cast(K.shape(yolo_outputs[0])[1:3] * 32,K.dtype(y_true[0]))
+        # 每个网络的尺寸，组成列表
+        grid_shapes = [K.cast(K.shape(yolo_outputs[l])[1:3],K.dtype(y_true[0])) for l in range(num_layers)]
+
+        m = K.shape(yolo_outputs[0])[0] # batch size 
+        mf = K.cast(m,K.dtype(yolo_outputs[0]))
+        loss = 0.0
+
+        for l in range(num_layers):
+            object_mask = y_true[l][...,4:5]
+            true_class_probs = y_true[l][...,5:]
+
+            grid,raw_pred,pred_xy,pred_wh = self.yolo_head(yolo_outputs[i],
+                    anchors[anchor_mask[l]],num_classes,input_shape,calc_loss = True)
+            pred_box = K.concatenate([pred_xy,pred_wh])
+
+            #darknet raw box to calculate loss.
+            raw_true_xy = y_true[l][...,:2] * grid_shapes[l][::-1] - grid
+            raw_true_wh = K.log(y_true[l][...,2:4] / anchors[anchor_mask[l]] * input_shape[::-1])
+            raw_true_wh = K.switch(object_mask,raw_true_wh,K.zeros_like(raw_true_wh)) # avoid log(0)=-inf
+            box_loss_scale = 2 - y_true[l][...,2:3] * y_true[l][...,3:4]
+
+            #find ingore mask,iterate over each of batch
+            ignore_mask = tf.TensorArray(K.dtype(y_true[0]),size =1,dynamic_size = True)
+            object_mask_bool = K.cast(object_mask,'bool')
+
+            def loop_body(b,ignore_mask):
+                true_box = tf.boolean_mask(y_true[l][b,...,0:4],object_mask_bool[b,...,0])
+                iou = box_iou(pred_box[b],true_box)
+                best_iou = K.max(iou,axis=-1)
+                ignore_mask = ingore_mask.write(b,K.cast(best_iou < ignore_thresh),K.dtype(true_box))
+                return b+1,ignore_mask
+            _, ignore_mask = K.control_flow_ops.while_loop(lambda b, *args: b < m, loop_body, [0, ignore_mask])
+            ignore_mask = ignore_mask.stack()
+            ignore_mask = K.expand_dims(ignore_mask, -1)
+
+            #binary_crossentropy 是 sigmoid_cross_entropy_with_logits
+            #这里raw_pred是feats 还没有进行sigmoid，然后用的交叉熵，而没有用 平方差，
+            #为什么没有用平方差呢？需要查查 交叉熵和方差代价函数的不同
+            xy_loss = object_mask * box_loss_scale * K.binary_crossentropy(raw_true_xy, raw_pred[..., 0:2],
+                                                                       from_logits=True)
+            wh_loss = object_mask * box_loss_scale * 0.5 * K.square(raw_true_wh - raw_pred[..., 2:4])
--- tool/preprocess.py
+++ tool/preprocess.py
@@ -18,12 +18,12 @@ def preprocess_true_boxes(true_boxes,input_shape,anchors,num_classes):
     '''
     assert(true_boxes[...,4] < num_classes).all(),'class id must be less than num_classes'
     num_layers = len(anchors)//3
-    anchor_mask = [[6,7,8], [3,4,5], [0,1,2]] if num_layers==3 else [[3,4,5], [1,2,3]]
+    anchor_mask = [[6,7,8], [3,4,5], [0,1,2]] if num_layers==3 else [[3,4,5], [0,1,2]]
 
     true_boxes = np.array(true_boxes,dtype = 'float32')
     input_shape = np.array(input_shape,dtype='int32')
     boxes_xy = (true_boxes[...,0:2] + true_boxes[...,2:4]) //2  #中心坐标(x,y)
-    boxes_wh = true_boxes[...,2:4] - true_boxes[...,0:2]        #宽高坐标(w,h)
+    boxes_wh = true_boxes[...,2:4] - true_boxes[...,0:2]        #宽高(w,h)
     #比如第0和1位设置为xy，除以416，归一化，第2和3位设置为wh，除以416，归一化，如[0.449, 0.730, 0.016, 0.026, 0.0]
     true_boxes[...,0:2] = boxes_xy / input_shape[::-1]  #input_shape 是hw,这里做个颠倒
     true_boxes[...,2:4] = boxes_wh / input_shape[::-1]
@@ -38,8 +38,43 @@ def preprocess_true_boxes(true_boxes,input_shape,anchors,num_classes):
                         (m,grid_shapes[l][0],grid_shapes[l][1],len(anchor_mask),5+num_classes),
                         dtype = 'float32') for l in range(num_layers)]
 
-    anchors = np.expand_dims(anchors,0)
+    anchors = np.expand_dims(anchors,0)  #由(9,2)转为(1,9,2)；
+    #算anchors_maxes mins 是为了计算IOU用的
+    #以0点为原点(-2/x,2/x)这样搞的最大最小值
     anchors_maxes = anchors/2.
     anchors_mins = - anchors_maxes
-    valid_mask = boxes_wh[...,0] >0   #将boxes_wh中宽w大于0的位，设为True，即含有box，结构是(m,T)
 
+    #将boxes_wh中宽w大于0的位，设为True，即含有box，结构是(m,T)
+    #作者这样设定是因为他定义了一个true_boxes的最大量比如20个，不足20个的用0填充了，超过20的截取了，为什么这么搞呢？
+    #可能是他觉得一张图的标记的时候目标不会多于20个？
+    valid_mask = boxes_wh[...,0] >0   
+
+    for b in range(m):
+        wh = boxes_wh[b,valid_mask]
+        if len(wh)==0: continue 
+
+        wh = np.expand_dims(wh,-2)  #是wh倒数第2个添加1位，如(*,2)->(*,1,2)  *是框的个数
+        box_maxes = wh/2.
+        box_mins = - box_maxes
+
+        #计算IOU,每个标注框box与9个anchor box 的Iou值(*,9)
+        intersect_mins  = np.maximum(box_mins,anchors_mins)
+        intersect_maxes = np.minimum(box_maxes,anchors_maxes)
+        intersect_wh = np.maximum(intersect_maxes - intersect_mins,0.)
+        intersect_area = intersect_wh[...,0] * intersect_wh[...,1]
+        box_area = anchors[...,0] * anchors[...,1]
+        iou = intersect_area / (box_area + anchor_area - intersect_area)
+
+        #为每一个true box 找最大IOU的anchor
+        best_anchor = np.argmax(iou,axis = -1)
+        for t,n in enumerate(best_anchor):
+            for l in range(num_layers):
+                if n in anchor_mask[l]:
+                    i = np.floor(true_boxes[b,t,0] * grid_shapes[l][1]).astype('int32')
+                    j = np.floor(true_boxes[b,t,0] * grid_shapes[l][0]).astype('int32')
+                    k = anchor_mask[l].index(n)
+                    c = true_boxes[b,t,4].astype('int32')
+                    y_true[l][b,j,i,k,0:4] = true_boxes[b,t,0:4]
+                    y_true[l][b,j,i,k,4] = 1 
+                    y_true[l][b,j,i,k,5+c] = 1
+        return y_true
